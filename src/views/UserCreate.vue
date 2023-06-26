485efca770a2e9a3d9f1fab20cdb65bf9e4d1eab
@satomi1 <satomi@fignny.co.jp>
2023-06-26 13:37:58 +0900

    train 3 validation

--- src/views/UserCreate.vue
+++ src/views/UserCreate.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { useConfirmUser } from '../stores/confirmUser'
 const okButtonClassName = ref('btn btn-dark btn-lg')
@@ -10,6 +10,13 @@ const confirmStore = useConfirmUser()
 
 const name = ref('名前')
 const mail = ref('example@com')
+const mailRegex = /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/
+const isMailValid = ref(false)
+const isButtonEnabled = computed(
+    () => {
+        return !isMailValid.value
+    }
+)
 
 function toConfirm(_name, _mail){
     confirmStore.setUserConfirm(_name, _mail)
@@ -20,16 +27,30 @@ function toConfirm(_name, _mail){
     router.push('/user/create/confirm')
 }
 
+watch(mail, () => {
+    if(mailRegex.test(mail.value)){
+        isMailValid.value = true
+    } else {
+        isMailValid.value = false
+    }
+})
+
 </script>
 
 <template>
     <div class="base-div">
         <div class="left-div">
-            <div class="cell-div"><label for="name-input">名前</label></div>
-            <div class="cell-div"><label for="name-mail">メールアドレス</label></div>
+            <div class="cell-label-div"><label for="name-input">名前</label></div>
+            <div class="cell-label-div"><label for="name-mail">メールアドレス</label></div>
         </div>
         <div class="center-div">
             <div class="cell-div"><input type="text"  v-model="name" id="name-input" name="name-input" size="20" /></div>
+            <div v-if="isMailValid" class="errorDiv">
+                <label></label>
+            </div>
+            <div v-else class="errorDiv">
+                <label>※メールアドレスの書式が正しくありません</label>
+            </div>
             <div class="cell-div"><input type="text"  v-model="mail" id="name-mail" name="name-mail" size="30" /></div>
         </div>
         <div class="right-div">
@@ -37,7 +58,7 @@ function toConfirm(_name, _mail){
     </div>
 
     <div class="action-ui-div">
-        <input v-bind:class="okButtonClassName" value="確認" v-on:click="toConfirm(name, mail)" style="margin-left:30px; margin-right:30px; width:130px;"/>
+        <input v-bind:class="okButtonClassName" v-bind:disabled="isButtonEnabled" value="確認" v-on:click="toConfirm(name, mail)" style="margin-left:30px; margin-right:30px; width:130px;"/>
         <router-link to="/user" v-bind:class="noButtonClassName" style="margin-left:30px; margin-right:30px; width:130px;">キャンセル</router-link>
     </div>
 </template>
@@ -67,11 +88,18 @@ function toConfirm(_name, _mail){
     margin-bottom: 50px;
 }
 .cell-div {
-    margin-top: 50px;
     margin-bottom: 50px;
+}
+.cell-label-div {
+    margin-bottom: 80px;
 }
 
 .action-ui-div {
     margin-left: 300px;
+}
+
+div.errorDiv {
+    margin: 0 0 0 0;
+    color: red;
 }
 </style>
--- src/views/UserUpdate.vue
+++ src/views/UserUpdate.vue
@@ -1,40 +1,66 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useUpdateConfirmUser } from '../stores/updateConfirmUser'
 
 const okButtonClassName = ref('btn btn-dark btn-lg')
 const noButtonClassName = ref('btn btn-dark btn-lg')
+const mailRegex = /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/
+const isMailValid = ref(false)
+const isButtonEnabled = computed(
+    () => {
+        return !isMailValid.value
+    }
+)
 
 const router = useRouter()
 const route = useRoute()
 const updateConfirmStore = useUpdateConfirmUser()
 updateConfirmStore.initUser(route.params.id)
 
+const mail = ref(updateConfirmStore.mail)
+
 function toConfirm(){
+    updateConfirmStore.$patch({
+        mail: this.mail
+    })
     router.push('/user/update/confirm')
 }
+
+watch(mail, () => {
+    if(mailRegex.test(mail.value)){
+        isMailValid.value = true
+    } else {
+        isMailValid.value = false
+    }
+})
 
 </script>
 
 <template>
     <div class="base-div">
         <div class="left-div">
-            <div class="cell-div"><label for="id-input">ID</label></div>
-            <div class="cell-div"><label for="name-input">名前</label></div>
-            <div class="cell-div"><label for="mail-input">メールアドレス</label></div>
+            <div class="cell-label-div"><label for="id-input">ID</label></div>
+            <div class="cell-label-div"><label for="name-input">名前</label></div>
+            <div class="cell-label-div"><label for="mail-input">メールアドレス</label></div>
         </div>
         <div class="center-div">
-            <div class="cell-div"><label>{{ updateConfirmStore.id }}</label></div>
+            <div class="cell-value-label-div"><label >{{ updateConfirmStore.id }}</label></div>
             <div class="cell-div"><input type="text" v-model="updateConfirmStore.name" id="name-input" name="name-input" size="20" /></div>
-            <div class="cell-div"><input type="text" v-model="updateConfirmStore.mail" id="mail-input" name="mail-input" size="30" /></div>
+            <div v-if="isMailValid" class="errorDiv">
+                <label></label>
+            </div>
+            <div v-else class="errorDiv">
+                <label>※メールアドレスの書式が正しくありません</label>
+            </div>
+            <div class="cell-div"><input type="text" v-model="mail" id="mail-input" name="mail-input" size="30" /></div>
         </div>
         <div class="right-div">
         </div>
     </div>
 
     <div class="action-ui-div">
-        <input v-bind:class="okButtonClassName" value="確認" v-on:click="toConfirm(name, mail)" style="margin-left:30px; margin-right:30px; width:130px;"/>
+        <input v-bind:class="okButtonClassName" v-bind:disabled="isButtonEnabled" value="確認" v-on:click="toConfirm(name, mail)" style="margin-left:30px; margin-right:30px; width:130px;"/>
         <router-link to="/user" v-bind:class="noButtonClassName" style="margin-left:30px; margin-right:30px; width:130px;">キャンセル</router-link>
     </div>
 </template>
@@ -64,10 +90,22 @@ function toConfirm(){
     margin-bottom: 50px;
 }
 .cell-div {
-    margin-top: 50px;
     margin-bottom: 50px;
+}
+
+.cell-label-div {
+    margin-bottom: 70px;
+}
+
+.cell-value-label-div{
+    margin-bottom: 65px;
 }
 .action-ui-div {
     margin-left: 300px;
+}
+
+div.errorDiv {
+    margin: 0 0 0 0;
+    color: red;
 }
 </style>
